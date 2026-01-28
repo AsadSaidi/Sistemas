4c570c44cd37b56571f4083bb65d8c0ae82012e5
AsadSaidi <asad.saidi.7e8@itb.cat>
2026-01-28 17:33:19 +0100

    P2.1 Creació d'un scritp

--- AsadSaidiTahere.sh
+++ AsadSaidiTahere.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
-arxiu = "info.txt"
+fitxer="info.txt"
 
-if [ -f "$arxiu"]; then 
-   echo "El fitxer $arxiu existeix"
+mostrar_menu() {
+    echo "1) Info bàsica del sistema (usuari, nom equip, kernel)"
+    echo "2) Info bàsica de disc i memòria"
+    echo "3) Informe complet (tot junt)"
+}
+
+if [ -f "$fitxer" ]; then
+   echo "El fitxer ja existeix. S'escriura al final del fitxer"
 else
-   echo "El fitxer $arxiu no existeix"
+   echo "Creant nou fitxer..."
+   nano info.txt
 fi
+
+echo "Informe generat el: $(date)" >> "$fitxer"
+echo "==============================" >> "$fitxer"
+
+mostrar_menu
+read -p "Escull una opció: " opcio
+
+case $opcio in
+    1) echo -e "Usuari: $(whoami)\nNom de l'equip: $(hostname)\nKernel: $(uname -r)" >> "$fitxer" ;;
+    2) echo -e "Disc:\n$(df -h)\nMemòria:\n$(free -h)" >> "$fitxer" ;;
+    3) 
+        echo -e "Usuari: $(whoami)\nNom de l'equip: $(hostname)\nKernel: $(uname -r)" >> "$fitxer"
+        echo -e "Disc:\n$(df -h)\nMemòria:\n$(free -h)" >> "$fitxer"
+        ;;
+    *) echo "Opció no vàlida. Sortint." ; exit 1 ;;
+esac
+
+echo "Fitxer generat correctament."
+echo "Informe guardat a $fitxer"
